228875bb7c314b98e268e61bae023c00dc00f5b8
eatPorkAndSeePigRun <811302964@qq.com>
2019-02-10 23:26:08 +0800

    upvalue.go

--- Source/luna/UpValue.go
+++ Source/luna/UpValue.go
@@ -2,4 +2,19 @@ package luna
 
 type UpValue struct {
 	GCObject
+	value Value
+}
+
+func (u UpValue) Accept(v GCObjectVisitor) {
+	if v.VisitUpValue(&u) {
+		u.value.Accept(v)
+	}
+}
+
+func (u *UpValue) SetValue(value *Value) {
+	u.value = *value
+}
+
+func (u UpValue) GetValue() *Value {
+	return &u.value
 }
