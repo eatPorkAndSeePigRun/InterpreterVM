08a05e1f2b60d0462e24f918d4d195e11f144511
eatPorkAndSeePigRun <811302964@qq.com>
2019-02-10 11:37:59 +0800

    string

--- Source/luna/String.go
+++ Source/luna/String.go
@@ -2,21 +2,55 @@ package luna
 
 type String struct {
 	GCObject
-	inHeap    string  // Calculate hash of string
+	inHeap    int32   // String in heap or not
 	strBuffer string  // Buffer for short string
 	str       *string // Pointer to heap which stored long string
 	length    uint64  // Length of string
-	hash      int64   // Hash value of string
+	hash_     int64   // Hash value of string
 }
 
-func (str String) hash_(s string)  {
-	
+// Calculate hash of string
+func (s String) hash(str string) {
+	s.hash_ = 5381
+	// TODO
 }
 
-func (str String) GetCStr() string {
+func (s String) Accept(v GCObjectVisitor) {
+	v.VisitString(&s)
+}
+
+func (s String) GetHash() int64 {
+	return s.hash_
+}
+
+func (s String) GetLength() uint64 {
+	return s.length
+}
+
+func (s String) GetCStr() string {
+	if s.inHeap != 0 {
+		return *s.str
+	} else {
+		return s.strBuffer
+	}
+}
+
+// Convert to string
+func (s String) GetStdString() string {
+	// TODO
+	//if s.inHeap != 0 {
+	//	return (*s.str) * s.length
+	//} else {
+	//	return s.strBuffer * s.length
+	//}
 	return ""
 }
 
-func (str String) GetStdString() string {
-	return ""
+// Change context of string
+func (s String) SetValue(str string) {
+	if s.inHeap != 0 {
+		s.str = nil
+	}
+	s.length = uint64(len(str))
+	// TODO
 }
